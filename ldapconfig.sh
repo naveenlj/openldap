504bf18331e3110f7c9f855b205f87f0f93853eb
Naveen <naveenlj@users.noreply.github.com>
2016-01-01 17:02:44 +0530

    Update ldapconfig.sh

--- ldapconfig.sh
+++ ldapconfig.sh
@@ -2,13 +2,13 @@
 
 set -x 
 
-echo "# specify the password generated above for "olcRootPW" section
+echo '# specify the password generated above for "olcRootPW" section
  dn: olcDatabase={0}config,cn=config
 changetype: modify
 add: olcRootPW
-olcRootPW: {SSHA}q2Y5Cm34ZaNanydqw8YrXoPlEoRmQtc1" > chrootpw.ldif
+olcRootPW: {SSHA}q2Y5Cm34ZaNanydqw8YrXoPlEoRmQtc1' | tee chrootpw.ldif
 
-echo "# specify the password generated above for olcRootPW section
+echo '# specify the password generated above for olcRootPW section
 dn: olcDatabase={0}config,cn=config
 changetype: modify
 add: olcRootPW
@@ -42,9 +42,9 @@ add: olcAccess
 olcAccess: {0}to attrs=userPassword,shadowLastChange by
   dn=cn=Manager,dc=nk,dc=solutions write by anonymous auth by self write by * none
 olcAccess: {1}to dn.base= by * read
-olcAccess: {2}to * by dn=cn=Manager,dc=nk,dc=solutions write by * read" > chdomain.ldif
+olcAccess: {2}to * by dn=cn=Manager,dc=nk,dc=solutions write by * read' | tee chdomain.ldif
 
-echo "# replace to your own domain name for dc=***,dc=*** section
+echo '# replace to your own domain name for dc=***,dc=*** section
 dn: dc=nk,dc=solutions
 objectClass: top
 objectClass: dcObject
@@ -63,5 +63,4 @@ ou: People
 
 dn: ou=Group,dc=nk,dc=solutions
 objectClass: organizationalUnit
-ou: Group
-" > basedomain.ldif
+ou: Group' | tee basedomain.ldif
